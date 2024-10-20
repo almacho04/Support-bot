eda37a5d0a7e7e6dfaf9678a82fc2f1350e2870b
almacho04 <95922652+almacho04@users.noreply.github.com>
2024-10-20 20:50:20 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -113,4 +113,4 @@ if __name__ == "__main__":
     # bot.set_webhook(url="https://1234abcd.ngrok.io/" + token)
 
     # Start Flask server on host 0.0.0.0 and port 8000
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=10000)
